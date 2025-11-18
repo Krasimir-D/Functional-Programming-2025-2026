ada52f9ad2cb477c7f6cd6e8d8678ab7b4a26a09
Krasimir Dyakov <80487068+Krasimir-D@users.noreply.github.com>
2025-11-18 09:05:19 +0200

    Update higher_orderism_is_in_order.hs

--- learn_you_a_haskell/higher_order_functions/higher_orderism_is_in_order.hs
+++ learn_you_a_haskell/higher_order_functions/higher_orderism_is_in_order.hs
@@ -1,1 +1,9 @@
+--Functions can take functions as arguments and can also return functions. To illustrate this, we're going to make a function that takes a function as parameter and
+--applies it twice
 
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+--First of all, notice the type declaration here. Before we didn't need parentheses, because "->" is naturaly right-associative. However, here they're mandatory.
+--They indicate that the first parameter is a function that takes something and return the same thing. We can read the type declaration of our function in the 
+--curried way, but to save ourselves the headache, we'll just say that the function takes two parameters and returns one thing.
